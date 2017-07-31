c2e7a5c075fb904565b8f21456c3755b0757f675
crbadboy <webworldcommunities@gmail.com>
2017-07-31 15:24:01 -0400

    Optimizing

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,9 +1,7 @@
 import { syncRoute} from '../shared/utils/routes.js';
 
 // TODO: If all routes in end up having a async counterpart, we can use normalmodulereplacement on this and possibly consolidate
-const Home = syncRoute(require('../shared/pages/home/index.jsx'), 'Home');
-
-const About = syncRoute(require('../client/pages/about/index.jsx'), 'About');
+import { Home,  About } from './route-components.js';
 
 export default {
     routes: [
